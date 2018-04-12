70598e89fb8145e93ba9ef217c95e9dded4729f7
unknown <londonvayavong@gmail.com>
2018-04-11 20:27:01 -0400

    added background image to notfound and added images folder

--- client/src/pages/NotFoundPage.js
+++ client/src/pages/NotFoundPage.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Background from '../images/blimp1.png';
+var sectionStyle = {
+  width: "100%",
+  height: "1300px",
 
+  backgroundImage: `url(${Background})`
+};
 class NotFoundPage extends Component {
   render() {
     return (
       <div>
-        Page not found.
+        <section style={sectionStyle}>
+        </section>
+        <div id="404" className="404">
+          <p>404</p>
+        </div>
       </div>
     );
   }
